7eb3e1a2c862774424b23bd7b3995092a3bc5dbb
Tohui <Tohuito@outlook.com>
2025-02-11 13:34:57 -0600

    Repository CRUD listo

--- src/modules/products/products.repository.ts
+++ src/modules/products/products.repository.ts
@@ -9,8 +9,37 @@ export  class productsRepository {
     return rows.length > 0 ? rows[0] : null;
    }
 
+   private async queryRows<T extends RowDataPacket>(sql:string,params:any[]):Promise<T[]>{
+      const [rows] = await pool.query<T[]>(sql,params)
+      return rows
+   }
+
+   private async queryExecute(sql: string, params: any[]): Promise<void> {
+      await pool.query(sql, params);
+  }
+  
+
+
+
    async findById(id:string){
     return await this.queryOne("select * from items where id = ?",[id])
+   }
+
+   async findAll(){
+      return await this.queryRows('Select * from items', [])
+   }
+
+   async deleteById (id:string){
+      await this.queryExecute('DELETE FROM items WHERE id = ? ',[id])
+      
+   }
+
+   async create(name:string, description:string){
+      await this.queryExecute('INSERT INTO items (name, description) values (?, ?)',[name, description])
+   }
+
+   async update(id:string, name:string, description:string){
+      await this.queryExecute("UPDATE items SET name = ?, description = ? WHERE id = ?",[name, description, id])
    }
 
 
--- src/sandboxRepository.ts
+++ src/sandboxRepository.ts
@@ -1,14 +1,65 @@
 import { productsRepository } from './modules/products/products.repository';
 
+const errorMessage = (error:(any | unknown)) => {
+    console.log(`Error al ejecutar la función: ${error}`)
+}
+
+//==============================================================
+
 const prueba = async () => {
-    const repo = new productsRepository();
+    const repository = new productsRepository();
 
     try {
-        const result = await repo.findById('2'); // Cambia el ID según lo que quieras probar
+        const result = await repository.findById('2'); // Cambia el ID según lo que quieras probar
         console.log(result); // Muestra el resultado en consola
     } catch (error) {
         console.error('Error al ejecutar la función:', error);
     }
 };
 
-prueba();
+const prueba2 = async() => {
+    const repository = new productsRepository
+    try {
+        const result = await repository.findAll()
+        console.log(result)
+        
+    } catch (error) {
+        errorMessage(error)
+    }
+}
+
+const prueba3 = async () => {
+    const repository = new productsRepository;
+    try {
+        await repository.deleteById('2')
+    } catch (error) {
+        errorMessage(error)
+    }
+}
+
+const prueba4 = async () => {
+    const repository = new productsRepository;
+    try {
+        await repository.create('Papitas','Con queso')
+    } catch (error) {
+        
+    }
+}
+
+const prueba5 = async () => {
+    const repository = new productsRepository
+    try {
+        await repository.update('7','Doritos','Con jalapeno')
+    } catch (error) {
+        
+    }
+}
+
+//prueba();
+//prueba4()
+
+//prueba3()
+
+//prueba5()
+prueba2()
+
